dd765afa8bf7a991b351480780fbc75d36cd25aa
hungwei <hungwei@LAPTOP-6VIAIBS1.>
2025-01-23 17:23:41 +0800

    Added cout

--- macros/Pedestals_with_BV/Pedestals_with_BV.cpp
+++ macros/Pedestals_with_BV/Pedestals_with_BV.cpp
@@ -119,6 +119,7 @@ int main() {
         ch_number[c] = c * 1.0 + 1.0;
         Pedestals[file_number][m][c] = h_ADC[m][c]->GetMean();
         Total_Noise[file_number][m][c] = h_ADC[m][c]->GetStdDev();
+        std::cout << Total_Noise[file_number][m][c] << std::endl;
         Pedestals_Error[file_number][m][c] = h_ADC[m][c]->GetMeanError();
       }
     }
